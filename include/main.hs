806f6c0d76764c63652d8fdf28fe6e419e926e83
Lee-Way Wang <lee.way56@comcast.net>
2022-04-19 16:35:17 -0700

    Print additional text in intro

--- include/main.hs
+++ include/main.hs
@@ -3,14 +3,32 @@ import System.IO
 
 
 print_border :: Int -> IO()
-print_border n = putStr "*"
+print_border 0 = putStrLn ""
+print_border n = do
+    putStr "*"
+    print_border (n - 1)
 
 
 print_intro :: IO()
 print_intro = do
-    print "LWOrganizationsISupport is a Haskell program that displays the NGOs and human"
-    print "rights organizations I support"
-    print_border 1
+    print_border 100
+    putStrLn "LWOrganizationsISupport is a Haskell program that displays the NGOs and human"
+    putStrLn "rights organizations I support.\n"
+    
+    putStrLn "As a big believer in the importance of human rights, I want to share the"
+    putStrLn "organizations that I have donated to.\n"
+    
+    putStrLn "Type \"begin\" to get started!"
+
+    input <- getLine
+    if input == "begin"
+        then putStrLn "we're proceeding"
+        else putStrLn "we're not proceeding"
+
+
+    print_border 100
+    
+
 
 main = do
     print_intro
